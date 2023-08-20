aa312ba268cac6f9d03c0885f6012052b0c3a059
collins <kipkorirc583@gmail.com>
2023-08-20 22:01:29 +0300

    First commit

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,12 @@
-#!/usr/bin/env python3
-
 def print_fibonacci(length):
-    pass
+    fib_sequence = [0, 1]
+
+    while len(fib_sequence) < length:
+        next_num = fib_sequence[-1] + fib_sequence[-2]
+        fib_sequence.append(next_num)
+
+    formatted_sequence = ', '.join(map(str, fib_sequence))
+    print(f"[{formatted_sequence}]")
+
+
+print_fibonacci(9)
